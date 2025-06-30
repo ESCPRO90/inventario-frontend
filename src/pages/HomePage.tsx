a10966c97be5134c49acc93fc171b02d18b04bbd
ESCPRO90 <77174672+ESCPRO90@users.noreply.github.com>
2025-06-30 11:15:28 -0600

    Delete src directory

--- src/pages/HomePage.tsx
+++
@@ -1,6 +0,0 @@
-// src/pages/HomePage.tsx
-function HomePage() {
-  return <div className='p-4 text-xl'>Sistema de Inventario Médico</div>
-}
-export default HomePage
-
--- src/services/productoService.ts
+++
@@ -1,6 +0,0 @@
-import axiosInstance from './axiosConfig'
-
-export const getProductos = async () => {
-  const res = await axiosInstance.get('/productos')
-  return res.data
-}
